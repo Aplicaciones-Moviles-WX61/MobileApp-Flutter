2b8bfa16c0cadd018b76fdbe25c8418b64d62dfe
John Leon <leonsahir7@gmail.com>
2022-10-26 22:44:24 -0500

    change auth views

--- lib/auth/login.dart
+++ lib/auth/login.dart
@@ -19,135 +19,141 @@ class LoginStateful extends State<Login> {
     return Scaffold(
       backgroundColor: Colors.white,
       body: SafeArea(
-          child: Padding(
-        padding: const EdgeInsets.all(24.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
+          child: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(24.0),
+          child: SizedBox(
+            height: MediaQuery.of(context).size.height,
+            width: MediaQuery.of(context).size.width,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
-                const Text(
-                  'Sing In',
-                  style: TextStyle(
-                      fontSize: 32,
-                      fontWeight: FontWeight.bold,
-                      color: Color(0xff2699FB)),
-                ),
-                Row(
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
                     const Text(
-                      'Don\'t have an account?',
-                      style: TextStyle(fontSize: 16, color: Color(0xff2699FB)),
+                      'Sing In',
+                      style: TextStyle(
+                          fontSize: 32,
+                          fontWeight: FontWeight.bold,
+                          color: Color(0xff2699FB)),
                     ),
-                    TextButton(
-                      child: const Text(
-                        'Sign Up',
-                        style: TextStyle(
-                            fontSize: 16,
-                            fontWeight: FontWeight.bold,
-                            color: Color(0xff2699FB)),
-                      ),
-                      onPressed: () {
-                        Navigator.pushReplacement(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => const Register()));
-                      },
-                    ),
-                  ],
-                ),
-              ],
-            ),
-            Column(
-              children: [
-                Container(
-                  padding: const EdgeInsets.fromLTRB(0, 10, 0, 10),
-                  child: TextField(
-                    controller: emailController,
-                    decoration: const InputDecoration(
-                      border: UnderlineInputBorder(),
-                      icon: Icon(Icons.email),
-                      labelText: 'E-mail',
-                    ),
-                  ),
-                ),
-                Container(
-                  padding: const EdgeInsets.fromLTRB(0, 10, 0, 10),
-                  child: TextField(
-                    obscureText: true,
-                    controller: passwordController,
-                    decoration: const InputDecoration(
-                      border: UnderlineInputBorder(),
-                      icon: Icon(Icons.lock),
-                      labelText: 'Password',
-                    ),
-                  ),
-                ),
-                Container(
-                    height: 50,
-                    width: double.infinity,
-                    padding: const EdgeInsets.fromLTRB(0, 10, 0, 0),
-                    child: ElevatedButton(
-                      child: const Text('Login'),
-                      onPressed: () {
-                        Navigator.pushReplacement(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => const Index()));
-                      },
-                    )),
-              ],
-            ),
-            Column(children: [
-              Container(
-                padding: const EdgeInsets.all(20),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: const [
-                    Image(
-                      image: AssetImage('assets/logos/gg.png'),
-                    ),
-                    Image(
-                      image: AssetImage('assets/logos/fb.png'),
-                    ),
-                    Image(
-                      image: AssetImage('assets/logos/tw.png'),
-                    )
-                  ],
-                ),
-              ),
-              Container(
-                  padding: const EdgeInsets.fromLTRB(0, 10, 0, 0),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      const Text(
-                        'Forgot password?',
-                        style:
-                            TextStyle(fontSize: 16, color: Color(0xff2699FB)),
-                      ),
-                      TextButton(
-                        child: const Text(
-                          'Reset',
-                          style: TextStyle(
-                              fontSize: 16,
-                              fontWeight: FontWeight.bold,
-                              color: Color(0xff2699FB)),
+                    Row(
+                      children: [
+                        const Text(
+                          'Don\'t have an account?',
+                          style:
+                              TextStyle(fontSize: 16, color: Color(0xff2699FB)),
                         ),
-                        onPressed: () {
-                          Navigator.pushReplacement(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) =>
-                                      const ForgotPassword()));
-                        },
-                      )
-                    ],
-                  ))
-            ]),
-          ],
+                        TextButton(
+                          child: const Text(
+                            'Sign Up',
+                            style: TextStyle(
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold,
+                                color: Color(0xff2699FB)),
+                          ),
+                          onPressed: () {
+                            Navigator.pushReplacement(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => const Register()));
+                          },
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+                Column(
+                  children: [
+                    Container(
+                      padding: const EdgeInsets.fromLTRB(0, 10, 0, 10),
+                      child: TextField(
+                        controller: emailController,
+                        decoration: const InputDecoration(
+                          border: UnderlineInputBorder(),
+                          icon: Icon(Icons.email),
+                          labelText: 'E-mail',
+                        ),
+                      ),
+                    ),
+                    Container(
+                      padding: const EdgeInsets.fromLTRB(0, 10, 0, 10),
+                      child: TextField(
+                        obscureText: true,
+                        controller: passwordController,
+                        decoration: const InputDecoration(
+                          border: UnderlineInputBorder(),
+                          icon: Icon(Icons.lock),
+                          labelText: 'Password',
+                        ),
+                      ),
+                    ),
+                    Container(
+                        height: 50,
+                        width: double.infinity,
+                        padding: const EdgeInsets.fromLTRB(0, 10, 0, 0),
+                        child: ElevatedButton(
+                          child: const Text('Login'),
+                          onPressed: () {
+                            Navigator.pushReplacement(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => const Index()));
+                          },
+                        )),
+                  ],
+                ),
+                Column(children: [
+                  Container(
+                    padding: const EdgeInsets.all(20),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: const [
+                        Image(
+                          image: AssetImage('assets/logos/gg.png'),
+                        ),
+                        Image(
+                          image: AssetImage('assets/logos/fb.png'),
+                        ),
+                        Image(
+                          image: AssetImage('assets/logos/tw.png'),
+                        )
+                      ],
+                    ),
+                  ),
+                  Container(
+                      padding: const EdgeInsets.fromLTRB(0, 10, 0, 0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          const Text(
+                            'Forgot password?',
+                            style: TextStyle(
+                                fontSize: 16, color: Color(0xff2699FB)),
+                          ),
+                          TextButton(
+                            child: const Text(
+                              'Reset',
+                              style: TextStyle(
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.bold,
+                                  color: Color(0xff2699FB)),
+                            ),
+                            onPressed: () {
+                              Navigator.pushReplacement(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) =>
+                                          const ForgotPassword()));
+                            },
+                          )
+                        ],
+                      ))
+                ]),
+              ],
+            ),
+          ),
         ),
       )),
     );
--- lib/auth/register.dart
+++ lib/auth/register.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:nextparty/auth/forgot_password.dart';
@@ -28,157 +30,32 @@ class RegisterStateful extends State<Register> {
           child: SingleChildScrollView(
               child: Padding(
             padding: const EdgeInsets.all(24.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    const Text(
-                      'Sign Up',
-                      style: TextStyle(
-                          fontSize: 32,
-                          fontWeight: FontWeight.bold,
-                          color: Color(0xff2699FB)),
-                    ),
-                    Row(
-                      children: [
-                        const Text(
-                          'Already have an account?',
-                          style:
-                              TextStyle(fontSize: 16, color: Color(0xff2699FB)),
-                        ),
-                        TextButton(
-                          child: const Text(
-                            'Sign In',
-                            style: TextStyle(
-                                fontSize: 16,
-                                fontWeight: FontWeight.bold,
-                                color: Color(0xff2699FB)),
-                          ),
-                          onPressed: () {
-                            Navigator.pushReplacement(
-                                context,
-                                MaterialPageRoute(
-                                    builder: (context) => const Login()));
-                          },
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
-                Column(
-                  children: [
-                    Container(
-                      padding: const EdgeInsets.fromLTRB(0, 10, 0, 10),
-                      child: TextField(
-                        controller: nameController,
-                        decoration: const InputDecoration(
-                          border: UnderlineInputBorder(),
-                          labelText: 'Name',
-                        ),
+            child: SizedBox(
+              height: MediaQuery.of(context).size.height - window.padding.top,
+              // width: MediaQuery.of(context).size.width,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const Text(
+                        'Sign Up',
+                        style: TextStyle(
+                            fontSize: 32,
+                            fontWeight: FontWeight.bold,
+                            color: Color(0xff2699FB)),
                       ),
-                    ),
-                    Container(
-                      padding: const EdgeInsets.fromLTRB(0, 10, 0, 10),
-                      child: TextField(
-                        controller: lastNameController,
-                        decoration: const InputDecoration(
-                          border: UnderlineInputBorder(),
-                          labelText: 'Lastname',
-                        ),
-                      ),
-                    ),
-                    Container(
-                      padding: const EdgeInsets.fromLTRB(0, 10, 0, 10),
-                      child: TextField(
-                        controller: emailController,
-                        decoration: const InputDecoration(
-                          border: UnderlineInputBorder(),
-                          icon: Icon(Icons.email),
-                          labelText: 'E-mail',
-                        ),
-                      ),
-                    ),
-                    Container(
-                      padding: const EdgeInsets.fromLTRB(0, 10, 0, 10),
-                      child: TextField(
-                        obscureText: true,
-                        controller: passwordController,
-                        decoration: const InputDecoration(
-                          border: UnderlineInputBorder(),
-                          icon: Icon(Icons.lock),
-                          labelText: 'Password',
-                        ),
-                      ),
-                    ),
-                    Container(
-                      padding: const EdgeInsets.fromLTRB(0, 10, 0, 10),
-                      child: TextField(
-                        controller: phoneController,
-                        keyboardType: TextInputType.phone,
-                        decoration: const InputDecoration(
-                          border: UnderlineInputBorder(),
-                          icon: Icon(Icons.phone),
-                          labelText: 'Phone',
-                        ),
-                      ),
-                    ),
-                    Container(
-                      padding: const EdgeInsets.fromLTRB(0, 10, 0, 10),
-                      child: TextField(
-                        controller: dateController,
-                        readOnly: true,
-                        decoration: const InputDecoration(
-                          border: UnderlineInputBorder(),
-                          icon: Icon(Icons.calendar_today),
-                          labelText: 'Birthday',
-                        ),
-                        onTap: () async {
-                          DateTime? pickedDate = await showDatePicker(
-                              context: context,
-                              initialDate: DateTime.now(),
-                              firstDate: DateTime(2000),
-                              lastDate: DateTime(2101));
-                          dateController.text = DateFormat('dd/MM/yyyy')
-                              .format(pickedDate!)
-                              .toString();
-                        },
-                      ),
-                    ),
-                    Container(
-                        height: 50,
-                        width: double.infinity,
-                        padding: const EdgeInsets.fromLTRB(0, 10, 0, 0),
-                        child: ElevatedButton(
-                          child: const Text('Register'),
-                          onPressed: () {
-                            print('----Login button pressed----');
-                            print('Name: ${nameController.text}');
-                            print('Lastname: ${lastNameController.text}');
-                            print('E-mail: ${emailController.text}');
-                            print('Phone: ${phoneController.text}');
-                            print('Password: ${passwordController.text}');
-                            print('Birthday: ${dateController.text}');
-                          },
-                        )),
-                  ],
-                ),
-                Column(children: [
-                  Container(
-                      padding: const EdgeInsets.fromLTRB(0, 10, 0, 0),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
+                      Row(
                         children: [
                           const Text(
-                            'Forgot password?',
+                            'Already have an account?',
                             style: TextStyle(
                                 fontSize: 16, color: Color(0xff2699FB)),
                           ),
                           TextButton(
                             child: const Text(
-                              'Reset',
+                              'Sign In',
                               style: TextStyle(
                                   fontSize: 16,
                                   fontWeight: FontWeight.bold,
@@ -188,14 +65,145 @@ class RegisterStateful extends State<Register> {
                               Navigator.pushReplacement(
                                   context,
                                   MaterialPageRoute(
-                                      builder: (context) =>
-                                          const ForgotPassword()));
+                                      builder: (context) => const Login()));
                             },
-                          )
+                          ),
                         ],
-                      ))
-                ]),
-              ],
+                      ),
+                    ],
+                  ),
+                  Column(
+                    children: [
+                      Container(
+                        padding: const EdgeInsets.fromLTRB(0, 0, 0, 10),
+                        child: TextField(
+                          controller: nameController,
+                          decoration: const InputDecoration(
+                            border: UnderlineInputBorder(),
+                            labelText: 'Name',
+                          ),
+                        ),
+                      ),
+                      Container(
+                        padding: const EdgeInsets.fromLTRB(0, 10, 0, 10),
+                        child: TextField(
+                          controller: lastNameController,
+                          decoration: const InputDecoration(
+                            border: UnderlineInputBorder(),
+                            labelText: 'Lastname',
+                          ),
+                        ),
+                      ),
+                      Container(
+                        padding: const EdgeInsets.fromLTRB(0, 10, 0, 10),
+                        child: TextField(
+                          controller: emailController,
+                          decoration: const InputDecoration(
+                            border: UnderlineInputBorder(),
+                            icon: Icon(Icons.email),
+                            labelText: 'E-mail',
+                          ),
+                        ),
+                      ),
+                      Container(
+                        padding: const EdgeInsets.fromLTRB(0, 10, 0, 10),
+                        child: TextField(
+                          obscureText: true,
+                          controller: passwordController,
+                          decoration: const InputDecoration(
+                            border: UnderlineInputBorder(),
+                            icon: Icon(Icons.lock),
+                            labelText: 'Password',
+                          ),
+                        ),
+                      ),
+                      Container(
+                        padding: const EdgeInsets.fromLTRB(0, 10, 0, 10),
+                        child: TextField(
+                          controller: phoneController,
+                          keyboardType: TextInputType.phone,
+                          decoration: const InputDecoration(
+                            border: UnderlineInputBorder(),
+                            icon: Icon(Icons.phone),
+                            labelText: 'Phone',
+                          ),
+                        ),
+                      ),
+                      Container(
+                        padding: const EdgeInsets.fromLTRB(0, 10, 0, 10),
+                        child: TextField(
+                          controller: dateController,
+                          readOnly: true,
+                          decoration: const InputDecoration(
+                            border: UnderlineInputBorder(),
+                            icon: Icon(Icons.calendar_today),
+                            labelText: 'Birthday',
+                          ),
+                          onTap: () async {
+                            DateTime? pickedDate = await showDatePicker(
+                                context: context,
+                                initialDate: DateTime.now(),
+                                firstDate: DateTime(2000),
+                                lastDate: DateTime(2101));
+                            dateController.text = DateFormat('dd/MM/yyyy')
+                                .format(pickedDate!)
+                                .toString();
+                          },
+                        ),
+                      ),
+                      Container(
+                          height: 50,
+                          width: double.infinity,
+                          padding: const EdgeInsets.fromLTRB(0, 10, 0, 0),
+                          child: ElevatedButton(
+                            child: const Text('Register'),
+                            onPressed: () {
+                              // print('----Login button pressed----');
+                              // print('Name: ${nameController.text}');
+                              // print('Lastname: ${lastNameController.text}');
+                              // print('E-mail: ${emailController.text}');
+                              // print('Phone: ${phoneController.text}');
+                              // print('Password: ${passwordController.text}');
+                              // print('Birthday: ${dateController.text}');
+                              print(MediaQuery.of(context).size.height -
+                                  window.padding.top);
+                              print(MediaQuery.of(context).size.height);
+                            },
+                          )),
+                    ],
+                  ),
+                  Column(children: [
+                    Container(
+                        padding: const EdgeInsets.fromLTRB(0, 10, 0, 0),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            const Text(
+                              'Forgot password?',
+                              style: TextStyle(
+                                  fontSize: 16, color: Color(0xff2699FB)),
+                            ),
+                            TextButton(
+                              child: const Text(
+                                'Reset',
+                                style: TextStyle(
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.bold,
+                                    color: Color(0xff2699FB)),
+                              ),
+                              onPressed: () {
+                                Navigator.pushReplacement(
+                                    context,
+                                    MaterialPageRoute(
+                                        builder: (context) =>
+                                            const ForgotPassword()));
+                              },
+                            )
+                          ],
+                        ))
+                  ]),
+                ],
+              ),
             ),
           )),
         ));
