c7a23791ef09fac611fbaed77187a1527894e04a
jubenacp <juben.200@hotmail.com>
2022-11-09 01:36:34 -0500

    item-detail view added

--- lib/menu/party_view.dart
+++ lib/menu/party_view.dart
@@ -5,6 +5,8 @@ import 'package:nextparty/menu/profile.dart';
 import 'package:nextparty/menu/settings.dart';
 import 'package:nextparty/menu/search.dart';
 
+import 'item_detail.dart';
+
 class Party extends StatefulWidget {
   const Party({super.key});
 
@@ -87,7 +89,7 @@ class PartyStateful extends State<Party> {
                   ),
                 ),
               ),
-              Container(
+              Container( //aca te quedaste
                   width: double.infinity,
                   padding: const EdgeInsets.only(
                       bottom: 10, top: 10, left: 10, right: 10),
@@ -105,53 +107,64 @@ class PartyStateful extends State<Party> {
                   height: 100,
                   margin: EdgeInsets.only(bottom: 5, top: 5),
                   padding: EdgeInsets.only(left: 25, right: 25),
-                  child: Container(
-                    decoration: BoxDecoration(
-                      color: Color(0xffBCE0FD),
-                      borderRadius: BorderRadius.circular(8),
-                    ),
-                    padding: EdgeInsets.only(left: 20, top: 10, bottom: 10),
-                    child: Row(
-                      children: [
-                        Container(
-                          width: 50,
-                          decoration: const BoxDecoration(
-                              image: DecorationImage(
-                                  // fit: BoxFit.cover,
-                                  image:
+                  child: InkWell(
+                    child: Container(
+                        decoration: BoxDecoration(
+                          color: Color(0xffBCE0FD),
+                          borderRadius: BorderRadius.circular(8),
+                        ),
+                        padding: EdgeInsets.only(left: 20, top: 10, bottom: 10),
+                        child: Row(
+                          children: [
+                            Container(
+                              width: 50,
+                              decoration: const BoxDecoration(
+                                  image: DecorationImage(
+                                    // fit: BoxFit.cover,
+                                      image:
                                       AssetImage('assets/products/ron.png'))),
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.all(10.0),
-                          child: Container(
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: const [
-                                Text(
-                                  'Ron Cartavio (1L)',
-                                  style: TextStyle(
-                                      fontSize: 20,
-                                      fontWeight: FontWeight.bold,
-                                      color: Colors.white),
-                                ),
-                                SizedBox(height: 5),
-                                Text(
-                                  'Cantidad:   3 unidades',
-                                  style: TextStyle(
-                                      fontSize: 15,
-                                      fontWeight: FontWeight.bold,
-                                      color: Colors.white),
-                                ),
-                              ],
                             ),
-                          ),
-                        ),
-                        Container(
-                            //icons
+                            Padding(
+                              padding: const EdgeInsets.all(10.0),
+                              child: Container(
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: const [
+                                    Text(
+                                      'Ron Cartavio (1L)',
+                                      style: TextStyle(
+                                          fontSize: 20,
+                                          fontWeight: FontWeight.bold,
+                                          color: Colors.white),
+                                    ),
+                                    SizedBox(height: 5),
+                                    Text(
+                                      'Cantidad:   3 unidades',
+                                      style: TextStyle(
+                                          fontSize: 15,
+                                          fontWeight: FontWeight.bold,
+                                          color: Colors.white),
+                                    ),
+
+                                  ],
+                                ),
+                              ),
+                            ),
+                            Container(
+                              //icons
                             )
-                      ],
+                          ],
+                        )
                     ),
-                  )),
+                      onTap: (){
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => const ItemDetail())
+                        );
+                      }
+                  )
+              ),
             ],
           ),
         ),
